ec709669011a7f720234721bc98311a0f77eba70
Luigi Alcabelu <alcabelu.hotmail.com>
2025-06-05 20:31:59 -0600

    correct isinglenton y value

--- MVCInjectDependecy6/MVCInjectDependecy6/Startup.cs
+++ MVCInjectDependecy6/MVCInjectDependecy6/Startup.cs
@@ -27,7 +27,7 @@ namespace MVCInjectDependecy6
 
             services.AddTransient<ITransient, OpenTelemetry>();
             services.AddScoped<IScoped, OpenTelemetry>();
-            services.AddSingleton<ISingleton, OpenTelemetry>();
+            services.AddSingleton<ISinglenton, OpenTelemetry>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
