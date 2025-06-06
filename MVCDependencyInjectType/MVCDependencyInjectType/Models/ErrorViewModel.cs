86edf4417050487a9f9f65b9d3da4e708bf95689
Luigi Alcabelu <alcabelu.hotmail.com>
2025-06-05 19:12:26 -0600

    MVC tipos de Inject Dependency

---
+++ MVCDependencyInjectType/MVCDependencyInjectType/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDependencyInjectType.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVCInjectDependecy6/MVCInjectDependecy6/OpenTelemetry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MVCInjectDependecy6
+{
+    public interface ITransient
+    { 
+    }
+
+    public interface IScoped
+    {
+    }
+
+    public interface ISinglenton    
+    {
+    }
+
+    public class OpenTelemetry : ITransient, ISinglenton, IScoped
+    {
+        public readonly int Value;
+        public OpenTelemetry() { 
+            Value = new Random().Next(56);
+
+        }
+
+    }
+}
